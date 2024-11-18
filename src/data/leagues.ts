5e79f4d6997222d0d77e0c420db4491c04651e8c
Julian Andres Pereira <jpereirap@unal.edu.co>
2024-11-17 19:41:59 -0500

    Add League logos

--- src/data/leagues.ts
+++ src/data/leagues.ts
@@ -3,38 +3,38 @@ import { LeaguePrev } from "../models/interfaces/interfaces";
 export const leagues: LeaguePrev[] = [
   {
     uid: "champions-league",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/3/3c/Uefa_Champions_League_logo_2.svg",
+    logo: "UefaChampionsLeague",
     name: "UEFA Champions League",
-    color: "#00529F",
+    color: "#0d3aff",
   },
   {
     uid: "premier-league",
-    logo: "https://upload.wikimedia.org/wikipedia/en/f/f2/Premier_League_Logo.svg",
+    logo: "PremierLeague",
     name: "Premier League",
     color: "#37003C",
   },
   {
     uid: "la-liga",
-    logo: "https://upload.wikimedia.org/wikipedia/en/9/92/La_Liga_Santander_logo.svg",
+    logo: "LaLiga",
     name: "La Liga",
     color: "#EF1C27",
   },
   {
     uid: "serie-a",
-    logo: "https://upload.wikimedia.org/wikipedia/en/e/e1/Serie_A_logo.svg",
+    logo: "SerieA",
     name: "Serie A",
-    color: "#009CDE",
+    color: "#171d8d",
   },
   {
     uid: "bundesliga",
-    logo: "https://upload.wikimedia.org/wikipedia/en/d/df/Bundesliga_logo_%282017%29.svg",
+    logo: "Bundesliga",
     name: "Bundesliga",
     color: "#CC0000",
   },
   {
     uid: "ligue-1",
-    logo: "https://upload.wikimedia.org/wikipedia/en/a/a7/Ligue_1_Uber_Eats_logo.svg",
+    logo: "Ligue1",
     name: "Ligue 1",
-    color: "#1C1C1C",
+    color: "#262624",
   },
 ];
